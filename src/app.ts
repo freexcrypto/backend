6bf1fb05310c89d7ec8f244ed355bb319b25224e
Zaky Irsyad Rais <zkyxentertain@gmail.com>
2025-05-12 18:15:38 +0700

    Update Vercel configuration to route API requests and refactor app to export instead of listen on a port

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,4 @@ app.use("/api/business", businessRoutes);
 app.use("/api/order", orderRoutes);
 app.use("/api/payment-link", paymentLinkRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+export default app;
